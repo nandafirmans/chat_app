76b6e1fbb193db6e47da7dfc280fe39515e14cc4
nandafirmans <nandafirmans@gmail.com>
2020-12-30 21:49:58 +0700

    init

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  static const routeName = "/auth";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
